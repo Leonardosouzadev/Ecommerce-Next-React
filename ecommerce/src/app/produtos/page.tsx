0b55796b8851a8b05a94db52f51354e968aec86a
Leonardosouzadev <leonardosouzadev03@gmail.com>
2025-03-05 19:30:13 -0300

    definindo tipagem para produtos

--- ecommerce/src/app/produtos/page.tsx
+++ ecommerce/src/app/produtos/page.tsx
@@ -1,13 +1,28 @@
-export default function Home() {
+import { ProductType } from "../../../types/producttype";
+
+async function getProdutos(): Promise<Product[]> {
+  const response = await fetch("https://fakestoreapi.com/products");
+
+  if (!response.ok) {
+    throw new Error("Erro ao buscar os produtos");
+  }
+
+  return response.json();
+}
+
+export default async function Home() {
+  const products: ProductType[] = await getProdutos();
   return (
-    <div className="container mx-auto px-4">
-      <div className="flex justify-between items-center py-4">
-        <div className="text-2xl font-bold">Produto1</div>
-        <div className="text-2xl font-bold">Produto2</div>
-        <div className="text-2xl font-bold">Produto3</div>
-        <div className="text-2xl font-bold">Produto4</div>
-        <div className="text-2xl font-bold">Produto5</div>
-        <div className="text-2xl font-bold">Produto6</div>
+    <div className="container mx-auto px-4 p-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {products.map((product: ProductType) => (
+          <div
+            key={product.id}
+            className="bg-gray-100 p-6 rounded-lg shadow-lg text-center"
+          >
+            {product.title}
+          </div>
+        ))}
       </div>
     </div>
   );
